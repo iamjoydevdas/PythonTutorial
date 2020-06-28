f94d95d1e5c01d4ab2869787651957f9106065cc
Joydev Das <iamjoydevdas@gmail.com>
2020-06-28 12:09:49 +0530

    basics tutorial

---
+++ Basics/BasicsTutorial/syntax/PrintAndComments.py
@@ -0,0 +1,16 @@
+'''
+Created on Jun 28, 2020
+
+@author: USE
+'''
+from email._header_value_parser import Comment
+
+#This is a simple commnet
+print("Hello World");
+
+"""
+This is 
+Multiline 
+Comment
+"""
+print("This is multi line comment")
